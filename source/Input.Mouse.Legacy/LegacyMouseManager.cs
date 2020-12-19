4a162a5502f94d1093abcb263a9dc89f92a48ff2
Nathan Alden, Sr <1189759+nathan-alden-sr@users.noreply.github.com>
2020-12-18 19:15:24 -0500

    Migrated Input.Mouse.Legacy project

---
+++ source/Input.Mouse.Legacy/LegacyMouseManager.cs
@@ -0,0 +1,77 @@
+using NathanAldenSr.VorpalEngine.Common;
+using static TerraFX.Interop.Windows;
+
+namespace NathanAldenSr.VorpalEngine.Input.Mouse.Legacy
+{
+    /// <summary>Manages mouse input based on legacy window messages.</summary>
+    public class LegacyMouseManager : MouseManager, ILegacyMouseManager
+    {
+        private Vector2<int>? _oldLocation;
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmMouseMove(nint lParam)
+        {
+            var newLocation = new Vector2<int>(LOWORD((uint)lParam), HIWORD((uint)lParam));
+
+            if (_oldLocation is object)
+            {
+                RelativeLocationXDelta += newLocation.X - _oldLocation.Value.X;
+                RelativeLocationYDelta += newLocation.Y - _oldLocation.Value.Y;
+            }
+
+            _oldLocation = newLocation;
+        }
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmLButtonDown() => SetButtonDown(Button.Left, true);
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmLButtonUp() => SetButtonDown(Button.Left, false);
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmMButtonDown() => SetButtonDown(Button.Middle, true);
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmMButtonUp() => SetButtonDown(Button.Middle, false);
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmRButtonDown() => SetButtonDown(Button.Right, true);
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmRButtonUp() => SetButtonDown(Button.Right, false);
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmXButtonDown(nuint wParam)
+        {
+            switch (HIWORD((uint)wParam))
+            {
+                case XBUTTON1:
+                    SetButtonDown(Button.X1, true);
+                    break;
+                case XBUTTON2:
+                    SetButtonDown(Button.X2, true);
+                    break;
+            }
+        }
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmXButtonUp(nuint wParam)
+        {
+            switch (HIWORD((uint)wParam))
+            {
+                case XBUTTON1:
+                    SetButtonDown(Button.X1, false);
+                    break;
+                case XBUTTON2:
+                    SetButtonDown(Button.X2, false);
+                    break;
+            }
+        }
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmMouseWheel(nuint wParam) => RelativeWheelDelta += unchecked((short)HIWORD((uint)wParam));
+
+        /// <inheritdoc />
+        public void UpdateStateFromWmMouseHWheel(nuint wParam) => RelativeHorizontalWheelDelta += unchecked((short)HIWORD((uint)wParam));
+    }
+}
